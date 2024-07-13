4edb1af4b05696c5f97bbb1c9fc422aceb24c662
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2024-07-13 16:44:02 -0400

    setup to intial view

---
+++ ecomsite/shop/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    discountPrice = models.FloatField()
+    category = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.CharField(max_length=300)
+
+
+
