ea1b6076b4787ab89eeef31c0220670fe937311e
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2024-08-01 10:13:44 -0400

    add queryset logic

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -4,5 +4,16 @@ from .models import Product,Order
 admin.site.site_header = 'E-commerce Site'
 admin.site.site_title = 'ABC Shopping'
 admin.site.index_title = 'Manage ABC Shopping'
-admin.site.register(Product)
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category='default')
+
+    change_category_to_default.short_description = 'Default Category'
+    list_display = ('title', 'price',  'category', 'discountPrice', 'description')
+    search_fields = ('title',)
+    actions=('change_category_to_default',)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Order)
--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    def __str__(self):
+        return self.title
     title = models.CharField(max_length=200)
     price = models.FloatField()
     discountPrice = models.FloatField()
